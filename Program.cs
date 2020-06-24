d9c97984def98fe6d11d2460da8a1f6bee5a4ead
Vinicius <viniciusokaeda@gmail.com>
2020-06-24 20:06:16 -0300

    estrutura inicial em andamento

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AULA24OBJARGUMENTO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
